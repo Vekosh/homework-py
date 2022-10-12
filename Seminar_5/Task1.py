30ea6f012b9cc0a9cf1c3b2f9f00dcd497793756
Valeriy Kudaev <Walera.ru11@mail.ru>
2022-10-12 17:10:24 +0300

    Homework

--- Seminar_5/Task1.py
+++ Seminar_5/Task1.py
@@ -20,21 +20,3 @@ def simple_sentence(word: str) -> str:
 res=random_words(int(input('Enter the number of words: ')))
 print(res)
 print(simple_sentence(res))
-
-# from random import sample
-
-
-# def list_rand_words(count: int, alp: str = 'абв'):
-#     words_list = []
-#     for i in range(count):
-#         letters = sample(alp, 3)
-#         words_list.append("".join(letters))
-#     return " ".join(words_list)
-
-# def simple_sentence(words: str) -> str:
-#     return " ".join(i for i in words.split() if i != "абв")
-
-
-# all_list = list_rand_words(int(input("Number of words: ")))
-# print(all_list)
-# print(simple_sentence(all_list))
