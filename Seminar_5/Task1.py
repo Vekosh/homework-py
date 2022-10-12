49311f25eb862b74f9bd93cff99b3ea2a5b9982a
Valeriy Kudaev <Walera.ru11@mail.ru>
2022-10-12 17:07:53 +0300

    Homework

---
+++ Seminar_5/Task1.py
@@ -0,0 +1,40 @@
+# 1. Напишите программу, удаляющую из текста все слова, содержащие "абв". В тексте используется разделитель пробел.
+# in
+# Number of words: 10
+# out
+# авб абв бав абв вба бав вба абв абв абв
+# авб бав вба бав вба
+
+from random import sample
+
+def random_words(quantity : int, word: str = "абв"):  
+    words_list = []
+    for i in range(quantity):
+        el = sample(word, 3)
+        words_list.append(''.join(el))
+    return ' '.join(words_list)
+
+def simple_sentence(word: str) -> str:
+    return ' '.join(i for i in word.split() if i != "абв")
+
+res=random_words(int(input('Enter the number of words: ')))
+print(res)
+print(simple_sentence(res))
+
+# from random import sample
+
+
+# def list_rand_words(count: int, alp: str = 'абв'):
+#     words_list = []
+#     for i in range(count):
+#         letters = sample(alp, 3)
+#         words_list.append("".join(letters))
+#     return " ".join(words_list)
+
+# def simple_sentence(words: str) -> str:
+#     return " ".join(i for i in words.split() if i != "абв")
+
+
+# all_list = list_rand_words(int(input("Number of words: ")))
+# print(all_list)
+# print(simple_sentence(all_list))
